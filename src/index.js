dc9d55b22f58d71c0d9b9404bdeee1fc0a3e2dae
Sonal <engineersonal@rediffmail.com>
2020-12-30 17:57:52 +0530

    Integrated details page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import Controller from "./screens/Controller";
 import "typeface-roboto";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Controller />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Controller />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
